1182981fd6506f81884bc34660ea468b1de6bd99
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-07-21 18:39:42 -0300

    punto 1
    Co-authored-by: Nicolas Ezequiel Pipke <npipke@frba.utn.edu.ar>
    Co-authored-by: bartolomefamea <bfamea@frba.utn.edu.ar>

--- src/tp.pl
+++ src/tp.pl
@@ -10,6 +10,24 @@
 % TESTS DEL PUNTO 1
 % ------------------------------------------------------------
 
+test(kanne_esta_viva_en_1370):-
+    esta_vivo(kanne, 1370).
+
+test(kanne_no_esta_viva_en_1300, [fail]):-
+    esta_vivo(kanne, 1300).
+
+test(kanne_no_esta_viva_en_2000, [fail]):-
+    esta_vivo(kanne, 2000).
+
+test(voll_esta_vivo_en_1550):-
+    esta_vivo(voll, 1550).
+
+test(voll_no_esta_vivo_en_1551, [fail]):-
+    esta_vivo(voll, 1551).
+
+test(serie_esta_viva_en_5000, [nondet]):-
+    esta_vivo(serie, 5000).
+
 
 % ------------------------------------------------------------
 % TESTS DEL PUNTO 2
@@ -35,7 +53,37 @@
 
 % Punto 1.a - Habitantes
 
+% habitante(nobre, raza, anio_naciomiento, pueblo ).
+habitante(denken, humano, 1290, auberst).
+habitante(voll, enano, 1200, ende).
+habitante(serie, elfo, 500, weise).     
+habitante(fern, humano, 1370, weise).
+habitante(stark, humano, 1368, riegel). 
+habitante(lawine, humano, 1372, auberst).
+habitante(kanne, humano, 1365, weise).
+habitante(wirbel, humano, 1350, klares).
+habitante(lernen, humano, 1315, auberst).
+habitante(frieren, elfo, 100, weise).
+habitante(eisen, enano, 1150, riegel).
+
+
+
+
 % Punto 1.b - Personas vivas según el año
+expectativa_vida(humano, 80).
+expectativa_vida(enano, 350).
+%expectativa_vida(elfo, _).
+
+esta_vivo(Persona, AnioConsulta):-
+    habitante(Persona, Raza, AnioNacimiento, _),
+    AnioNacimiento =< AnioConsulta,
+    sigue_vivo(Raza, AnioNacimiento, AnioConsulta).
+
+sigue_vivo(elfo, _, _).
+
+sigue_vivo(Raza, AnioNacimiento, AnioConsulta):-
+    expectativa_vida(Raza, Expectativa),
+    AnioConsulta =< AnioNacimiento + Expectativa.
 
 
 % ------------------------------------------------------------
@@ -70,5 +118,3 @@
 % Punto 3.f - Recuerdos generados por días festivos
 
 % Punto 3.g - Recuerdos generados por estatuas
-
-
