8e4eba90c7ef5a6f64ce684c8aaba86d14c4aa48
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-07-21 17:30:16 -0300

    separadores esteticos

--- src/tp.pl
+++ src/tp.pl
@@ -1,3 +1,74 @@
+% ============================================================
+% TESTS
+% ============================================================
+
+
 :- begin_tests(tpIntegrador, []).
 
+
+% ------------------------------------------------------------
+% TESTS DEL PUNTO 1
+% ------------------------------------------------------------
+
+
+% ------------------------------------------------------------
+% TESTS DEL PUNTO 2
+% ------------------------------------------------------------
+
+
+% ------------------------------------------------------------
+% TESTS DEL PUNTO 3
+% ------------------------------------------------------------
+
+
 :- end_tests(tpIntegrador).
+
+
+% ============================================================
+% PARTE 1 - PARTE 1 - PARTE 1 - PARTE 1 - PARTE 1 - PARTE 1
+% ============================================================
+
+
+% ------------------------------------------------------------
+% PUNTO 1: LA GENTE
+% ------------------------------------------------------------
+
+% Punto 1.a - Habitantes
+
+% Punto 1.b - Personas vivas según el año
+
+
+% ------------------------------------------------------------
+% PUNTO 2: LOS RECUERDOS
+% ------------------------------------------------------------
+
+% Punto 2.a - Hazañas conocidas por las personas
+
+% Punto 2.b - Duración de los recuerdos
+
+% Punto 2.c - Hazañas recordadas en un año
+
+% Punto 2.d - Hazañas corroboradas
+
+% Punto 2.e - Hazañas que pasaron al olvido
+
+
+% ------------------------------------------------------------
+% PUNTO 3: CONMEMORANDO HAZAÑAS
+% ------------------------------------------------------------
+
+% Punto 3.a - Días festivos
+
+% Punto 3.b - Estatuas
+
+% Punto 3.c - Mantenimientos de estatuas
+
+% Punto 3.d - Estado de conservación de las estatuas
+
+% Punto 3.e - Conocimiento de hazañas por conmemoraciones
+
+% Punto 3.f - Recuerdos generados por días festivos
+
+% Punto 3.g - Recuerdos generados por estatuas
+
+
