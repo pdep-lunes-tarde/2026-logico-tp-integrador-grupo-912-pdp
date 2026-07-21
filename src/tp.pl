25f3c48a0220fdef3f64046db6e3f277e75ad7c6
martinzaj <mzajdenberg@frba.utn.edu.ar>
2026-07-21 19:23:19 -0300

    punto 2
    Co-authored-by: bartolomefamea <bfamea@frba.utn.edu.ar>

--- src/tp.pl
+++ src/tp.pl
@@ -28,6 +28,15 @@ test(voll_no_esta_vivo_en_1551, [fail]):-
 test(serie_esta_viva_en_5000, [nondet]):-
     esta_vivo(serie, 5000).
 
+test(lawine_recuerda_destruir_al_demonio_aura_en_1400, [nondet]):-
+    es_recordada_por(hazania(destruir_al_demonio_aura, _, _), lawine, 1400).
+
+test(lawine_no_recuerda_destruir_al_demonio_aura_en_1380, [fail]):-
+    es_recordada_por(hazania(destruir_al_demonio_aura, _, _), lawine, 1380).
+
+test(lawine_no_recuerda_destruir_al_demonio_aura_en_1410, [fail]):-
+    es_recordada_por(hazania(destruir_al_demonio_aura, _, _), lawine, 1410).
+
 
 % ------------------------------------------------------------
 % TESTS DEL PUNTO 2
@@ -86,19 +95,57 @@ sigue_vivo(Raza, AnioNacimiento, AnioConsulta):-
     AnioConsulta =< AnioNacimiento + Expectativa.
 
 
+
 % ------------------------------------------------------------
 % PUNTO 2: LOS RECUERDOS
 % ------------------------------------------------------------
 
-% Punto 2.a - Hazañas conocidas por las personas
+% conoce(Persona, Hazania, AnioConocimiento, Forma).
+% conoce(Persona, hazania(Nombre_Hazania, [Realizadores_de_Hazania], Lugar_de_Hazania), AnioConocimiento, Forma).
+conoce(wirbel, hazania(rescatar_a_la_hermana_de_wirbel, [stark, fern], klares), 1390, presencio).
+conoce(frieren, hazania(rescatar_a_la_hermana_de_wirbel, [stark, fern], klares), 1390, presencio).
+conoce(lawine, hazania(destruir_al_demonio_aura, [frieren], weise), 1393, cancion).
+conoce(voll, hazania(destruir_al_demonio_aura, [denken], auberst), 1400, libro(50)).
+conoce(serie, hazania(destruir_al_rey_demonio, [frieren, himmel, heiter, eisen], ende), 1335, libro(100)).
+conoce(kanne, hazania(recuperar_al_gato_perdido, [himmel, frieren], weise), 1375, presencio).
 
-% Punto 2.b - Duración de los recuerdos
+% hazania(Nombre_Hazania, [Realizadores_de_Hazania], Lugar_de_Hazania)
 
-% Punto 2.c - Hazañas recordadas en un año
 
-% Punto 2.d - Hazañas corroboradas
+% Punto 2.a - Una hazaña es recordada por alguien en cierto año
 
-% Punto 2.e - Hazañas que pasaron al olvido
+es_recordada_por(Hazania, Persona, Anio):-
+    conoce(Persona, Hazania, AnioConocimiento, FormaConocimiento),
+    esta_vivo(Persona, Anio),
+    AnioConocimiento =< Anio,
+    recuerdo_vigente(FormaConocimiento, AnioConocimiento, Anio).
+
+recuerdo_vigente(presencio, _, _).
+
+recuerdo_vigente(cancion, AnioConocimiento, Anio):-
+    Anio =< AnioConocimiento + 15.
+
+recuerdo_vigente(libro(Paginas), AnioConocimiento, Anio):-
+    Anio =< AnioConocimiento + Paginas.
+
+
+% Punto 2.b - Una Hazania esta corroborada
+
+esta_corroborada(Nombre_Hazania):-
+    conoce(_, hazania(Nombre_Hazania, Realizadores, Lugar_de_Hazania), _, _),
+    not(hay_otra_version(Nombre_Hazania, Realizadores, Lugar_de_Hazania)).
+
+hay_otra_version(Nombre_Hazania, Realizadores, _):-
+    conoce(_, hazania(Nombre_Hazania, Otros_Realizadores, _), _, _),
+    Otros_Realizadores \= Realizadores.
+
+hay_otra_version(Nombre_Hazania, _, Lugar_de_Hazania):-
+    conoce(_, hazania(Nombre_Hazania, _, Otro_Lugar_de_Hazania), _, _),
+    Otro_Lugar_de_Hazania \= Lugar_de_Hazania.
+
+
+% Punto 2.c - Paso al olvido
+
 
 
 % ------------------------------------------------------------
@@ -117,4 +164,4 @@ sigue_vivo(Raza, AnioNacimiento, AnioConsulta):-
 
 % Punto 3.f - Recuerdos generados por días festivos
 
-% Punto 3.g - Recuerdos generados por estatuas
+% Punto 3.g - Recuerdos generados por estatuas.
